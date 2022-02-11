b8f1b41ca03ca6a101750bd75b62d5e97ce694f8
mikirupann <sw.miki.0907@icloud.com>
2022-02-11 16:24:41 +0900

    Flask準備おけ

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-def main():
-    pass
+# /indexにGETでアクセスがきたらindex.htmlを返す
+@app.route("/index")
+def index():
+    customers = [["Bob", 15],
+                 ["Tom", 57],
+                 ["Ken", 73]]
+    return render_template("index.html", customers=customers)
 
 
 if __name__ == '__main__':
