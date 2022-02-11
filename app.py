c087bf45838356e650e6bd94f8a3d53f3d47ee7a
mikirupann <sw.miki.0907@icloud.com>
2022-02-11 21:00:22 +0900

    諦め

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 import db
 
 app = Flask(__name__)
 
 
-# /indexにGETでアクセスがきたらindex.htmlを返す
-@app.route("/index")
+# /indexにGETでアクセスが来たらindex.htmlを返す
+@app.get("/index")
 def index():
     customers = db.get_all_customers()
     return render_template("index.html", customers=customers)
+
+
+@app.post("/index")
+def register():
+    name = request.form['name']
+    age = request.form['age']
+    db.add_customer(name, age)
+
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
